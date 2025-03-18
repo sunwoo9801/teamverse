e3705513303e9d35d9a0bebf4def5785402eeafc
sunwoo9801 <swookid9801@gmail.com>
2025-03-18 16:01:50 +0900

    Fix CORS issue

--- src/main/java/org/zerock/teamverse/config/WebConfig.java
+++ src/main/java/org/zerock/teamverse/config/WebConfig.java
@@ -25,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer  {
 				registry.addMapping("/api/**") // API 요청 경로만 CORS 허용
 				.allowedOrigins(
 					"http://localhost:3000",
-					"https://splendorous-sorbet-72e1b9.netlify.app" // Netlify 프론트엔드 허용
+					"https://amazing-starship-4e4e37.netlify.app" // Netlify 프론트엔드 허용
 				)				
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") // 허용할 HTTP 메서드
 				.allowedHeaders("*") // 모든 요청 헤더 허용
@@ -37,7 +37,7 @@ public class WebConfig implements WebMvcConfigurer  {
 				registry.addMapping("/uploads/**")
 				.allowedOrigins(
 					"http://localhost:3000",
-					"https://splendorous-sorbet-72e1b9.netlify.app" // Netlify 프론트엔드 허용
+					"https://amazing-starship-4e4e37.netlify.app" // Netlify 프론트엔드 허용
 				)					.allowedMethods("GET"); // ✅ GET 요청만 허용 (보안 목적)
 
 			}
